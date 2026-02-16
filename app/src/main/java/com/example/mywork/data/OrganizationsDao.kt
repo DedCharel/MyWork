e0f7b21c7d2e59b95202fff9fef7794968e91ac2
Daniil Shcherbakov <Danvikmail@gmail.com>
2026-02-16 15:39:09 +0300

    WorkersDao and OrganizationsDao

--- app/src/main/java/com/example/mywork/data/OrganizationsDao.kt
+++ app/src/main/java/com/example/mywork/data/OrganizationsDao.kt
@@ -1,8 +1,31 @@
 package com.example.mywork.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface OrganizationsDao {
+
+    @Transaction
+    @Query("SELECT * FROM organizations ORDER BY name DESC")
+    fun getOrganizations(): Flow<List<OrganizationDbModel>>
+
+    @Transaction
+    @Query("SELECT * FROM organizations WHERE organizationId ==:organizationId")
+    suspend fun getOrganization(organizationId: Long): OrganizationDbModel
+
+    @Transaction
+    @Query("DELETE FROM organizations WHERE organizationId ==:organizationId")
+    suspend fun deleteOrganization(organizationId: Long)
+
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addOrganization(organization: OrganizationDbModel)
+
+
 }
--- app/src/main/java/com/example/mywork/data/WorkersDao.kt
+++ app/src/main/java/com/example/mywork/data/WorkersDao.kt
@@ -4,14 +4,26 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WorkersDao {
 
+    @Transaction
     @Query("SELECT * FROM workers ORDER BY name DESC")
     fun getAllWorkers(): Flow<List<WorkerDbModel>>
 
+    @Transaction
+    @Query("SELECT * FROM workers WHERE workerId == :workerId")
+    suspend fun getWorker(workerId: Long): WorkerDbModel
+
+    @Transaction
+    @Query("DELETE FROM workers WHERE workerId == :workerId")
+    suspend fun deleteWorker(workerId: Long)
+
+    @Transaction
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addWorker(workerDao: WorkersDao)
+    suspend fun addWorker(worker: WorkerDbModel)
+
 }
