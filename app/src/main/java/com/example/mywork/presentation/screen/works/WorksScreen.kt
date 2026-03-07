00f2c714059b9eff7311607b11dfdb9efc486b22
Daniil Shcherbakov <Danvikmail@gmail.com>
2026-03-07 16:08:23 +0300

    Move Search to TopBar

--- app/src/main/java/com/example/mywork/presentation/screen/works/WorksScreen.kt
+++ app/src/main/java/com/example/mywork/presentation/screen/works/WorksScreen.kt
@@ -3,12 +3,16 @@ package com.example.mywork.presentation.screen.works
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -33,6 +37,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -57,19 +62,30 @@ fun WorksScreen(
     Scaffold(
         modifier = modifier,
         topBar = {
-            TopAppBar(
-                title = { Text(text = "Work list") },
-                actions = {
-                    Icon(
-                        modifier = Modifier
-                            .clip(CircleShape)
-                            .clickable { onSettingsClick() }
-                            .padding(end = 16.dp),
-                        imageVector = Icons.Default.Settings,
-                        contentDescription = "Settings"
-                    )
-                }
-            )
+            Column {
+                TopAppBar(
+                    title = { Text(text = "Work list") },
+                    actions = {
+                        Icon(
+                            modifier = Modifier
+                                .clip(CircleShape)
+                                .clickable { onSettingsClick() }
+                                .padding(end = 16.dp),
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = "Settings"
+                        )
+                    }
+                )
+                SearchBar(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    query = state.value.query,
+                    onQueryChange = {
+                        viewModel.processCommand(WorkCommand.InputSearchQuery(it))
+                    }
+                )
+            }
+
         },
         floatingActionButton = {
             FloatingActionButton(
@@ -85,31 +101,21 @@ fun WorksScreen(
             }
         }
     ) { innerPadding ->
-        LazyColumn(
-            contentPadding = innerPadding
-        ) {
-            item {
-                SearchBar(
-                    modifier = Modifier.padding(8.dp),
-                    query = state.value.query,
-                    onQueryChange = {
-                        viewModel.processCommand(WorkCommand.InputSearchQuery(it))
-                    }
-                )
-            }
-            item{
-                Spacer(Modifier.height(8.dp))
-            }
-            items(
-                items = currentState.works,
-                key = {it.id}
-            ){work->
-                WorkCard(
 
-                    work =work,
-                    onWorkClick = {onWorkClick(it.id)})
+
+            LazyColumn(
+                Modifier.padding(innerPadding)
+            ) {
+                items(
+                    items = currentState.works,
+                    key = { it.id }
+                ) { work ->
+                    WorkCard(
+
+                        work = work,
+                        onWorkClick = { onWorkClick(it.id) })
+                }
             }
-        }
     }
 }
 
